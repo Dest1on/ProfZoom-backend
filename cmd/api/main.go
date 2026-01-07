b035bb331ef1e2884b194e598dcdfb773dddb7a2
Dmitry Dem <dimixlive87@gmail.com>
2026-01-07 15:19:51 +0700

    Restructuring

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	apphttp "github.com/Dest1on/ProfZoom-backend/internal/http"
+)
+
+func main() {
+	router := apphttp.NewRouter()
+
+	log.Println("API started on :8080")
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
+}
